78648e6f1204da3dbe2797ce15888bfd4defad80
student <olgaru@gmail.com@gmail.com>
2019-11-23 12:05:56 +0200

    initial task requirements

---
+++ src/com/accenture/lecture15/ObjectPractice.java
@@ -0,0 +1,13 @@
+package com.accenture.lecture15;
+
+public class ObjectPractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
