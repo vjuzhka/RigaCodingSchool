0933d7132b1440fcad30a5f9051333bc385cf8bf
student <olgaru@gmail.com@gmail.com>
2019-11-27 19:53:08 +0200

    New Warrior

--- src/com/accenture/lecture16/Warrior.java
+++ src/com/accenture/lecture16/Warrior.java
@@ -3,6 +3,7 @@ package com.accenture.lecture16;
 public class Warrior {
 	private String name;
 	private int health;
+	private int damagePower;
 
 	public Warrior(String name, int initialHealth) {
 		this.name = name;
@@ -29,38 +30,43 @@ public class Warrior {
 	}
 
 	public void damage(int damage) {
-		if (damage > 0) {
-
-			System.out.println("Your damage is  " + damage + " Warrior: " + name);
-					
-
+			
+			if (isAlive()) {
+									
+			if ((health - damage) >= 0) {
+				health = health - damage;
+			 if (damage <0) {
+				System.out.println("Zero");
+												}
+			} else {
+				health = 0;
+			}
+			System.out.println("Your damage is  " + damage + " Warrior: "
+					+ name);
 		} else {
-			health = health - damage;
-
-			System.out.println("You are dead.");
+			System.out.println("Warrior alreay dead.");
 
 		}
+
 	}
 
 	public void restoreHealth(int health1) {
-		
-		if (health1 > 0) {
+
+		if (isAlive()) {
 			health1 = this.health + health;
 
-			System.out.println("Take some pills, it helps." );
-					
+			System.out.println("Take some pills, it helps.");
 
 		} else {
-						
+
 			System.out.println("Sorry, we can't help you :(");
 
 		}
-		
 
 	}
 
 	public String toString() {
-				return "Warrior alive:"+isAlive();
+		return "Dear " + name + "You have " + health + " points";
 	}
 
 }
--- src/com/accenture/lecture16/WarriorApp.java
+++ src/com/accenture/lecture16/WarriorApp.java
@@ -1,19 +1,30 @@
 package com.accenture.lecture16;
 
 public class WarriorApp {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Warrior warrior = new Warrior("God", 100);
-		warrior.toString();
 		warrior.damage(10);
 		warrior.restoreHealth(80);
-		System.out.println(warrior.toString() + " God is alive! ");
+		System.out.println(warrior);
 
+		Warrior warrior2 = new Warrior("Fedor", 80);
+		warrior2.damage(70);
+		warrior2.restoreHealth(10);
+		System.out.println(warrior2);
+
+		Warrior warrior3 = new Warrior("Sigizmund", 80);
+		warrior3.damage(80);
+		warrior3.restoreHealth(0);
+		System.out.println(warrior3);
+
+		Warrior warrior4 = new Warrior("Petrov", 80);
+		warrior4.damage(-100);
+		warrior4.restoreHealth(0);
+		warrior4.damage(-100);
+		System.out.println(warrior4);
+		
 	}
-
 }
